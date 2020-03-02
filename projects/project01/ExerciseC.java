69ea116e27a18f93342a38a29025b9d554b6a4fa
Sammy Samkough <sammysamkough@gmail.com>
2020-03-01 21:15:50 -0500

    Updated Exercise C

--- projects/project01/ExerciseC.java
+++ projects/project01/ExerciseC.java
@@ -2,20 +2,58 @@ package projects.project01;
 
 import java.util.Set;
 import java.util.HashSet;
+import java.util.List;
+import java.util.ArrayList;
 
+/* INCOMPLETE */
 public class ExerciseC {
-    public static Set<Set<Integer>> nSetCartesianProduct(int n) {
-        Set<Set<Integer>> result = new HashSet<>();
+    public static List<List<Integer>> nSetCartesianProduct(List<List<Integer>> setList) {
+        List<List<Integer>> cartesianProduct = new ArrayList<>();
+        int amountOfPerms = 1;
 
-        // create n sets
-        for (int i = 0; i < n; i++) {
+        for (List<Integer> list : setList) {
+            amountOfPerms *= list.size();
         }
 
-        return result;
+        for (int i = 0; i < amountOfPerms; i++) {
+            List<Integer> list = new ArrayList<>();
+            cartesianProduct.add(list);
+        }
+
+        for (List<Integer> list : setList) {
+            int listIndex = 0;
+            int indexLongevity = amountOfPerms / list.size();
+            int cart = 0;
+            for (int i = 0; i < cartesianProduct.size(); i++) {
+                if (listIndex == indexLongevity || indexLongevity == amountOfPerms) {
+                    listIndex = 0;
+                }
+                cartesianProduct.get(cart).add(list.get(listIndex));
+                cart++;
+                listIndex++;
+            }
+        }
+
+        return cartesianProduct;
     }
 
-    public static void main (String args[]) {
-        int n = 3;
-        System.out.println(nSetCartesianProduct(n));
+    public static void main(String args[]) {
+        List<Integer> A = new ArrayList<>();
+        A.add(1);
+        A.add(2);
+
+        List<Integer> B = new ArrayList<>();
+        B.add(3);
+        B.add(4);
+
+        List<Integer> C = new ArrayList<>();
+        C.add(5);
+
+        List<List<Integer>> setList = new ArrayList<>();
+        setList.add(A);
+        setList.add(B);
+        setList.add(C);
+
+        System.out.println(nSetCartesianProduct(setList));
     }
 }
--- projects/project01/WarmupB.java
+++ projects/project01/WarmupB.java
@@ -6,22 +6,23 @@ import java.util.List;
 import java.util.ArrayList;
 
 public class WarmupB {
-    public static Set<List<Integer>> twoSetCartesianProduct(List<Integer> A, List<Integer> B) {
-        Set<List<Integer>> C = new HashSet<>();
+    public static Set<List<Integer>> twoSetCartesianProduct(List<Integer> setA, List<Integer> setB) {
+        Set<List<Integer>> cartesianProduct = new HashSet<>();
 
-        for (int i : A) {
-            for (int j : B) {
-                List<Integer> D = new ArrayList<>();
-                D.add(i);
-                D.add(j);
-                C.add(D);
+        for (int i : setA) {
+            for (int j : setB) {
+                System.out.println("A: " + i + ", B: " + j);
+                List<Integer> setD = new ArrayList<>();
+                setD.add(i);
+                setD.add(j);
+                cartesianProduct.add(setD);
             }
         }
 
-        return C;
+        return cartesianProduct;
     }
 
-    public static void main (String args[]) {
+    public static void main(String args[]) {
         List<Integer> A = new ArrayList<>();
         A.add(1);
         A.add(2);
@@ -29,6 +30,10 @@ public class WarmupB {
         List<Integer> B = new ArrayList<>();
         B.add(3);
         B.add(4);
+
+        System.out.println(twoSetCartesianProduct(A, B));
+
+        B.add(5);
 
         System.out.println(twoSetCartesianProduct(A, B));
     }
